7a9f71c65edcf53ea0d54c20c8fb8ee79a27c005
Carlos Zambrano <emmanuelzzz123@gmail.com>
2023-06-06 10:03:49 -0700

    add images 6

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@ const APP_SHELL = [
   "img/avatars/23.png",
   "img/avatars/24.png",
   "img/avatars/25.png",
-  "img/avatars/26.png",
+  "img/avatars/starlord.jpg",
   "img/avatars/27.png",
   "img/avatars/28.png",
   "img/avatars/29.png",
